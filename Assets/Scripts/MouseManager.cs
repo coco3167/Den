7a7804a910645b469a3c6bddc60ae37ab1bcae64
Corentin <corentinboblet@gmail.com>
2025-04-08 09:46:04 +0200

    Keep the mouse on the ground

--- Assets/Scripts/MouseManager.cs
+++ Assets/Scripts/MouseManager.cs
@@ -5,19 +5,33 @@ public class MouseManager : MonoBehaviour
 {
     [SerializeField] private Rigidbody mouseRigidBody;
     [SerializeField] private float mouseSensitivity = 1f;
+    [SerializeField] private LayerMask terrainLayerMask;
     
     private void Awake()
     {
         Cursor.lockState = CursorLockMode.Locked;
+        
+        #if UNITY_EDITOR
         Mouse.current.WarpCursorPosition(Camera.main.ViewportToScreenPoint(new Vector3(0.5f, 0.5f, 0f))); 
+        #endif
     }
 
     private void OnMouseMoved(InputValue value)
     {
         if(!Application.isFocused)
             return;
+        
         Vector2 mouseDelta = value.Get<Vector2>() * mouseSensitivity * Time.deltaTime;
         Vector3 mousePos = mouseRigidBody.position + new Vector3(mouseDelta.x, mouseRigidBody.position.y, mouseDelta.y);
+        
+        // Keep the mouse on the ground
+        Physics.Raycast(mousePos + Vector3.up * 10f, Vector3.down, out RaycastHit hit, 100, terrainLayerMask);
+        if(hit.collider == null)
+            Physics.Raycast(mousePos + Vector3.down * 10f, Vector3.up, out hit, 100, terrainLayerMask);
+        if(hit.collider == null)
+            return;
+        mousePos.y = hit.point.y;
+        
         mouseRigidBody.MovePosition(mousePos);
     }
 
