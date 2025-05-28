e869caa09632ee996cf984a7aaaaa2f0111bcf56
Corentin <corentinboblet@gmail.com>
2025-05-28 10:56:38 +0200

    Added skeleton for movement animation integration

--- Assets/Scripts/SmartObjects_AI/Agent/AnimationAgent.cs
+++ Assets/Scripts/SmartObjects_AI/Agent/AnimationAgent.cs
@@ -2,19 +2,28 @@ using UnityEngine;
 
 namespace SmartObjects_AI.Agent
 {
-    [RequireComponent(typeof(Animator))]
+    [RequireComponent(typeof(Animator), typeof(MovementAgent))]
     public class AnimationAgent : MonoBehaviour
     {
+        // ID for animator
         private static readonly int StartUse = Animator.StringToHash("StartUse");
         private static readonly int FinishUse = Animator.StringToHash("FinishUse");
-        
+        private static readonly int Speed = Animator.StringToHash("Speed");
+
         private Animator m_animator;
+        private MovementAgent m_movementAgent;
 
         private bool m_isFinished = true;
         
         private void Awake()
         {
             m_animator = GetComponent<Animator>();
+            m_movementAgent = GetComponent<MovementAgent>();
+        }
+
+        private void Update()
+        {
+            m_animator.SetFloat(Speed, m_movementAgent.GetSpeed());
         }
 
         public void SwitchAnimator(RuntimeAnimatorController animatorController)
