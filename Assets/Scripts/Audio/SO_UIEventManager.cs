dcaec52b16a9b1af4d9d15c7b31068afc7f1a9e7
Corentin <corentinboblet@gmail.com>
2025-06-05 16:52:22 +0200

    Revert "UI cassée ?"
    This reverts commit 1ea4490a6080c3a3574241d9240f69547a3a690a.

--- Assets/Scripts/Audio/SO_UIEventManager.cs
+++ Assets/Scripts/Audio/SO_UIEventManager.cs
@@ -6,28 +6,27 @@ public class SO_UIEventManager : ScriptableObject
 {
     public static void PlayFaderTick()
     {
-        AudioManager.Instance.FaderTick.Post(GameManager.Instance.GetCamera().gameObject);
+        AudioManager.Instance.FaderTick.Post(Camera.main.gameObject);
     }
     public static void PlayBox()
     {
-        AudioManager.Instance.Box.Post(GameManager.Instance.GetCamera().gameObject);
+        AudioManager.Instance.Box.Post(Camera.main.gameObject);
     }
 
     public static void PlayMasterTest()
     {
-        Debug.Log(AudioManager.Instance.MasterTest);
-        AudioManager.Instance.MasterTest.Post(GameManager.Instance.GetCamera().gameObject);
+        AudioManager.Instance.MasterTest.Post(Camera.main.gameObject);
     }
     public static void PlayMusicTest()
     {
-        AudioManager.Instance.MusicTest.Post(GameManager.Instance.GetCamera().gameObject);
+        AudioManager.Instance.MusicTest.Post(Camera.main.gameObject);
     }
     public static void PlaySFXTest()
     {
-        AudioManager.Instance.SFXTest.Post(GameManager.Instance.GetCamera().gameObject);
+        AudioManager.Instance.SFXTest.Post(Camera.main.gameObject);
     }
     public static void PlayAmbienceTest()
     {
-        AudioManager.Instance.AmbienceTest.Post(GameManager.Instance.GetCamera().gameObject);
+        AudioManager.Instance.AmbienceTest.Post(Camera.main.gameObject);
     }
 }
