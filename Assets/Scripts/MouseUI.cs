c41b3e7c0a888309ce0fe4f0f6dd258a5ed2f45d
Corentin Boblet <corentinboblet@gmail.com>
2025-06-08 13:50:40 +0200

    Repaired UI for playtest + hides mouse cursor on pause

--- Assets/Scripts/MouseUI.cs
+++ Assets/Scripts/MouseUI.cs
@@ -1,11 +1,13 @@
+using System;
 using Sirenix.OdinInspector;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class MouseUI : MonoBehaviour
+public class MouseUI : MonoBehaviour, IPausable
 {
     [SerializeField, ChildGameObjectsOnly] private RectTransform rectTransform;
+    [SerializeField, ChildGameObjectsOnly] private RawImage rawImage;
     [SerializeField] private MouseManager mouseManager;
-
 
     private Camera m_camera;
     private Vector2 m_viewportSize;
@@ -20,5 +22,10 @@ public class MouseUI : MonoBehaviour
     {
         m_viewportSize = new Vector2(m_camera.scaledPixelWidth, m_camera.scaledPixelHeight);
         rectTransform.position = m_camera.WorldToViewportPoint(mouseManager.GetRawWorldMousePosition()) * m_viewportSize;
+    }
+
+    public void OnGamePaused(object sender, EventArgs eventArgs)
+    {
+        rawImage.enabled = !GameManager.Instance.IsPaused;
     }
 }
